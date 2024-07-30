b168a805157940092eb3d892d289c3b094f2916e
Isaac Njiru <isaac.njiru@gmail.com>
2024-07-29 22:33:31 -0600

    A given string represents a DNA sequence, The function counts how many of each nucleotide is present. If the string contains characters that aren't A, C, G, or T then it is invalid and you should signal an error.

---
+++ nucleotide-count/NucleotideCount.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+public static class NucleotideCount
+{
+   enum NucleotideEnum
+    {
+        A=0,
+        C=1,
+        G=2, 
+        T=3,
+
+    }
+    public static IDictionary<char, int> Count(string sequence)
+    {
+        char[] chars = sequence.ToCharArray();
+         Dictionary<char, int> nucleotideDict = new() { 
+             {'A',0 },
+             {'C',0 },
+             {'G',0 },
+             {'T',0 }
+         };
+
+        foreach (char _char in chars)
+        {
+            var _testCharEnum = Enum.Parse(typeof(NucleotideEnum), Char.ToString(_char));
+            var convertedChar = Convert.ToChar(_testCharEnum.ToString());
+
+            if (nucleotideDict.TryGetValue(Convert.ToChar(_testCharEnum.ToString()), out int value))
+            {
+                nucleotideDict[_char] = value + 1;
+            }
+          
+        }
+        return nucleotideDict;
+    }
+}
