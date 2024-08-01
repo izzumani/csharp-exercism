a4509959dd03f7d1f366f338fcccfd2b58ce0998
Isaac Njiru <isaac.njiru@gmail.com>
2024-08-01 17:35:18 -0600

    refactor to use queue instead

--- circular-buffer/CircularBuffer.cs
+++ circular-buffer/CircularBuffer.cs
@@ -4,76 +4,28 @@ using System.Linq;
 
 public class CircularBuffer<T>
 {
-    private  int[] _bufferArray;
-    private int currentIndex = -1;
-    private int currentWriteIndex = 0;
-    private int currentStartIndex = 0;
+  
+    private Queue<int> _bufferArray = new Queue<int>();
     private int _capacity = 0;
+
     public CircularBuffer(int capacity)
     {
-        
-        _bufferArray =Array.Empty<int>();
         _capacity = capacity;
-  
-
     }
 
-    public T Read()
-    {
-     
-        try
-        {
-            var bufferRead = (T)Convert.ChangeType(_bufferArray[0], typeof(int));
-
-            List<int> _bufferList = _bufferArray.ToList();
-            _bufferList.RemoveAt(0);
-            _bufferArray = _bufferList.ToArray();
-            currentWriteIndex -= 1;
-
-            return bufferRead;
-        }
-        catch (IndexOutOfRangeException)
-        {
-
-            throw new InvalidOperationException();
-        }
-        catch(Exception ex)
-        {
-            throw ex;
-        }
-        
-        //.
-
-    }
+    public T Read() => (T)Convert.ChangeType(_bufferArray.Dequeue(), typeof(int));
 
     public void Write(T value)
     {
-        Array.Resize(ref _bufferArray, _capacity);
-        try
-        { 
-            _bufferArray[currentWriteIndex] = (int)Convert.ChangeType(value, typeof(int));
-            currentWriteIndex += 1;
-
-
-        }
-        catch (IndexOutOfRangeException)
-        {
-
-            throw new InvalidOperationException();
-        }
-        catch (Exception ex)
-        {
-            throw ex;
-        }
-
-
+            if (_bufferArray.Count >= _capacity)
+                throw new InvalidOperationException();
+       
+            _bufferArray.Enqueue((int)Convert.ChangeType(value, typeof(int)));
     }
 
     public void Overwrite(T value)
     {
-        int emptyIndex =  Array.IndexOf(_bufferArray, 0);
-
-        if (emptyIndex ==-1)
+        if (_bufferArray.Count == _capacity)
         {
             Read();
             Write(value);
@@ -81,17 +33,13 @@ public class CircularBuffer<T>
         }
         else
         {
-            _bufferArray[emptyIndex] = (int)Convert.ChangeType(value, typeof(int));
-           
+            _bufferArray.Enqueue((int)Convert.ChangeType(value, typeof(int)));
+
         }
 
     }
 
-    public void Clear()
-    {
-        _bufferArray = Array.Empty<int>();
-        currentIndex = -1;
-        currentWriteIndex = 0;
+    public void Clear() =>_bufferArray.Clear();
 
-    }
+   
 }
